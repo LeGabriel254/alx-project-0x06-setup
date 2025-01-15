b31510121157665d8eac411210c1d8210f1d7157
Leon Gabriel <g73346920@gmail.com>
2025-01-15 13:51:48 +0300

    Chored:Modified.

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,44 +1,36 @@
 import Link from "next/link";
 import Button from "../common/Button";
-import { useCount } from "@/context/CountContext"
-import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname(); // Get the current pathname of the app
-  const { count } = useCount(); // Access the count value from the CountContext
+
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
     <header className="fixed w-full bg-white shadow-md">
-      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8 ">
-
-        {/* Logo linking to the homepage */}
-        <Link href='/' className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
         </Link>
 
         {/* Button Group */}
         <div className="flex gap-4">
           {
-
-            // Conditional rendering based on the current pathname
             !["/counter-app"].includes(pathname) ? (
-
               <>
-
-                {/*Show Sign In and Sign Up buttons if not on "/counter-app" */}
-                <Button
-                  buttonLabel="Sign In"
-                  buttonBackgroundColor="red"
-                />
-                <Button
-                  buttonLabel="Sign Up"
-                  buttonBackgroundColor="blue"
-                />
-              </>
-
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
             ) : (
-
-              // Show the current count if on "/counter-app"
               <p className=" font-semibold text-lg">Current count : {count}</p>
             )
           }
@@ -46,7 +38,7 @@ const Header: React.FC = () => {
         </div>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
